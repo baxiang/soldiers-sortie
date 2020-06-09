c91373eec7bc096f292bccc268f817a811c4ab0b
baxiang <yangyucug@gmail.com>
2020-06-10 01:10:33 +0800

    ./idea

--- gorpc/go.mod
+++ gorpc/go.mod
@@ -5,10 +5,7 @@ go 1.13
 require (
 	github.com/golang/protobuf v1.4.2
 	github.com/hashicorp/consul/api v1.4.0
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/opentracing/opentracing-go v1.1.0
-	github.com/stretchr/testify v1.6.1 // indirect
-	github.com/vmihailenco/msgpack v3.3.3+incompatible
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
 	google.golang.org/appengine v1.6.6 // indirect
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
---
+++ gorpc/http/transport.go
@@ -0,0 +1,66 @@
+package http
+
+import (
+	"context"
+	"net"
+	"net/http"
+
+	
+	
+)
+type httpServerTransport struct {
+	http.Server
+	opts *transport.ServerTransportOptions
+	Router *httprouter.Router // router for httpServerTransport
+}
+
+var DefaultRouter *httprouter.Router
+
+func init() {
+	// use httprouter for default router
+	DefaultRouter = httprouter.New()
+}
+
+// The default httpServerTransport
+var DefaultHttpServerTransport = NewHttpServerTransport()
+
+// Use the singleton pattern to create a server transport
+var NewHttpServerTransport = func() *httpServerTransport {
+	return &httpServerTransport{
+		opts : &transport.ServerTransportOptions{},
+	}
+}
+
+func init() {
+	transport.RegisterServerTransport("http", DefaultHttpServerTransport)
+}
+
+func (s *httpServerTransport) ListenAndServe(ctx context.Context, opts ...transport.ServerTransportOption) error {
+	for _, o := range opts {
+		o(s.opts)
+	}
+
+	lis, err := net.Listen(s.opts.Network, s.opts.Address)
+	if err != nil {
+		return err
+	}
+
+
+	s.Server.Handler = DefaultRouter
+	go func() {
+		if err = s.Server.Serve(lis); err != nil {
+			log.Errorf("http serve error, %v", err)
+		}
+	}()
+
+	return nil
+}
+
+// HandlerFunc is an adapter which allows the usage of an http handler
+// request handle.
+func HandleFunc(method, path string, handler func(http.ResponseWriter, *http.Request)) error {
+
+	DefaultRouter.HandlerFunc(method, path, handler)
+
+	return nil
+}
