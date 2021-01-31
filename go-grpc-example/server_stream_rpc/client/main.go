c50357d90e8129789428e480f39e020e3e435cbb
baxiang <yangyucug@gmail.com>
2021-01-31 18:21:33 +0800

    go-kit

--- go-grpc-example/server_stream_rpc/client/main.go
+++ go-grpc-example/server_stream_rpc/client/main.go
@@ -1,5 +1,61 @@
 package main
 
+import (
+	"context"
+	"io"
+	"log"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/baxiang/soldiers-sortie/go-grpc-example/proto"
+)
+
+const Address string = ":8000"
+
+var grpcClient pb.StreamServerClient
+
 func main() {
-	$END$
+	// 连接服务器
+	conn, err := grpc.Dial(Address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("net.Connect err: %v", err)
+	}
+	defer conn.Close()
+	// 建立gRPC连接
+	grpcClient = pb.NewStreamServerClient(conn)
+
+	listValue()
 }
+
+
+// listValue 调用服务端的ListValue方法
+func listValue() {
+
+
+	// 同时传入了一个 context.Context ，在有需要时可以让我们改变RPC的行为，比如超时/取消一个正在运行的RPC
+	stream, err := grpcClient.ListValue(context.Background(), &pb.SimpleReq{
+		Para: "stream server grpc ",
+	})
+	if err != nil {
+		log.Fatalf("Call ListStr err: %v", err)
+	}
+	for {
+		//Recv() 方法接收服务端消息，默认每次Recv()最大消息长度为`1024*1024*4`bytes(4M)
+		res, err := stream.Recv()
+		// 判断消息流是否已经结束
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("ListStr get stream err: %v", err)
+		}
+		// 打印返回值
+		log.Println(res.Val)
+		// break
+	}
+	// //可以使用CloseSend()关闭stream，这样服务端就不会继续产生流消息
+	// //调用CloseSend()后，若继续调用Recv()，会重新激活stream，接着之前结果获取消息
+	//stream.CloseSend()
+}
+
+
--- go-grpc-example/server_stream_rpc/server/main.go
+++ go-grpc-example/server_stream_rpc/server/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	pb "github.com/baxiang/soldiers-sortie/go-grpc-example/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"strconv"
+	"time"
+)
+
+var (
+	Address =":8000"
+)
+
+type StreamService struct {
+	pb.UnimplementedStreamServerServer
+}
+
+// ListValue 实现ListValue方法
+func (s *StreamService) ListValue(req *pb.SimpleReq, srv pb.StreamServer_ListValueServer) error {
+	for n := 0; n < 5; n++ {
+		// 向流中发送消息， 默认每次send送消息最大长度为`math.MaxInt32`bytes
+		err := srv.Send(&pb.StreamResp{
+			Val: req.Para + strconv.Itoa(n),
+		})
+		if err != nil {
+			return err
+		}
+		// log.Println(n)
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
 func main() {
-	$END$
+	listener, err := net.Listen("tcp", Address)
+	if err != nil {
+		log.Fatalf("net.Listen err: %v", err)
+	}
+	log.Println(Address + " net.Listing...")
+	// 新建gRPC服务器实例
+	// 默认单次接收最大消息长度为`1024*1024*4`bytes(4M)，单次发送消息最大长度为`math.MaxInt32`bytes
+	// grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(1024*1024*4), grpc.MaxSendMsgSize(math.MaxInt32))
+	grpcServer := grpc.NewServer()
+	// 在gRPC服务器注册我们的服务
+	pb.RegisterStreamServerServer(grpcServer, &StreamService{})
+
+	//用服务器 Serve() 方法以及我们的端口信息区实现阻塞等待，直到进程被杀死或者 Stop() 被调用
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("grpcServer.Serve err: %v", err)
+	}
 }
