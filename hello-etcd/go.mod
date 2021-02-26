bfcc3996bcf4259ad45130c1b13b09d1a756720d
baxiang <yangyucug@gmail.com>
2021-02-26 20:40:03 +0800

    etcd

--- hello-etcd/go.mod
+++ hello-etcd/go.mod
@@ -1,3 +1,14 @@
 module github.com/baxiang/soldiers-sortie/hello-etcd
 
 go 1.14
+
+require (
+	go.etcd.io/etcd v0.5.0-alpha.5.0.20200910180754-dd1b699fc489
+	google.golang.org/genproto v0.0.0-20201210142538-e3217bee35cc // indirect
+	google.golang.org/grpc v1.34.0 // indirect
+)
+
+replace (
+	go.etcd.io/etcd => go.etcd.io/etcd v0.5.0-alpha.5.0.20200910180754-dd1b699fc489 // ae9734ed278b is the SHA for git tag v3.4.13
+	google.golang.org/grpc => google.golang.org/grpc v1.27.1
+)
--- hello-etcd/put/main.go
+++ hello-etcd/put/main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/mvcc/mvccpb"
+)
+
 func main() {
-	$END$
+
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"}, // 集群列表
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 模拟etcd中kv的变化
+	go func() {
+		for {
+			client.Put(context.TODO(), "hello", "world")
+
+			client.Delete(context.TODO(), "hello")
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
+	// 启动监听 5秒后关闭
+	ctx, cancelFunc := context.WithCancel(context.TODO())
+	time.AfterFunc(10*time.Second, func() {
+		cancelFunc()
+	})
+	watchRespChan := client.Watch(ctx, "hello")
+
+	// 处理kv变化事件
+	for watchResp := range watchRespChan {
+		for _, event := range watchResp.Events {
+			switch event.Type {
+			case mvccpb.PUT:
+				fmt.Println("put action", string(event.Kv.Value))
+			case mvccpb.DELETE:
+				fmt.Println("delete action", string(event.Kv.Key))
+			}
+		}
+	}
 }
